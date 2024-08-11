02470f46cccdec7b545022de11c79e48eb834dce
arsalannury <arsiiiland@yahoo.com>
2024-08-11 12:19:40 +0330

    🎨

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
-package main
+package main;
 
-import "myApp/logger"
+import "myApp/logger";
 
 func main() {
 	logger.Logger(logger.Types{ERROR: "ERROR"},"The Error Has Occurd In Line 64");
+	logger.Logger(logger.Types{WARN: "WARN"},"The Warn Has Occurd In Line 102");
+	logger.Logger(logger.Types{LOG: "LOG"},"The LOG Has Occurd In Line 12");
 }
