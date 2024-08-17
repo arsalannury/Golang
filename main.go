41c5b39e5ee2500bf43c421b66c2318f65d91380
arsalannury <arsiiiland@yahoo.com>
2024-08-17 15:22:07 +0330

    train

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
-import (
-	"module/utils"
-)
+import "fmt"
 
 func main() {
-	//utils.DateToJalali(1999, 4, 19)
-	utils.JalaliMonth()
-	//var slice = []uint8{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
+	fmt.Println("Hello World !")
 }
