70fff8074e05d634fc4e0abfba2d17830e4964a7
ArsalanNury <arsiiiland@yahoo.com>
2024-09-03 00:04:19 +0330

    try to learn golang

---
+++ guests.go
@@ -0,0 +1,8 @@
+package main
+
+var family = [4]string{"Mother", "Father", "Brother", "Sister"}
+var guests = [4]string{"Ali", "Ahmad", "Sara", "Negar"}
+
+func guest() {
+	//return append(family, guests...)
+}
