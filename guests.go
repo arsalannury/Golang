ec5ab03c762eaff698f05d80a02799ee7cae311f
ArsalanNury <arsiiiland@yahoo.com>
2024-09-04 01:58:42 +0330

    amounts

--- guests.go
+++
@@ -1,8 +0,0 @@
-package main
-
-var family = [4]string{"Mother", "Father", "Brother", "Sister"}
-var guests = [4]string{"Ali", "Ahmad", "Sara", "Negar"}
-
-func guest() {
-	//return append(family, guests...)
-}
