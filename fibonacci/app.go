0b5cf48f1bd7d987f9dcb7791895dcaa89d395c2
arsalannury <arsiiiland@yahoo.com>
2024-10-05 17:12:33 +0330

    train

---
+++ fibonacci/app.go
@@ -0,0 +1,14 @@
+package fibonacci
+
+func Fibonacci() int {
+	var firstItems = [2]int{1, 2}
+	var current int = 0
+	var finalItems = []int{}
+
+	for {
+		var e = append(finalItems, firstItems[0]+firstItems[1])
+
+	}
+
+	return current
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module example.com/essential
+module core
 
 go 1.22.2
