37d9fe3185d002de7bcebfcc9ab7da56dde296e8
ArsalanNury <arsiiiland@yahoo.com>
2024-10-08 17:39:54 +0330

    fibonacci

--- fibonacci/app.go
+++ fibonacci/app.go
@@ -4,17 +4,16 @@ import "fmt"
 
 func Fibonacci() int {
 	var current int = 0
-	var finalItems = []int{1, 2}
+	var finalItems = [30]int{1, 2}
 
-	for i := 0; i <= len(finalItems); i++ {
-		fmt.Println(finalItems[1], "fdfsdfsd")
-		finalItems[len(finalItems)+i] = finalItems[len(finalItems)-1] + finalItems[len(finalItems)-2]
-		if len(finalItems) == 3000 {
+	fmt.Println(len(finalItems))
+	for i := 2; i <= len(finalItems); i++ {
+		fmt.Println(finalItems[i-2]+finalItems[i-1], "i1")
+
+		finalItems[i] = finalItems[i-2] + finalItems[i-1]
+		if finalItems[len(finalItems)-1] != 0 {
 			break
-
 		}
 	}
-	fmt.Println(finalItems)
-
 	return current
 }
