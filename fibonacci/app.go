6e935a423c46b175b36dc625c93c48f742dbed9c
ArsalanNury <arsiiiland@yahoo.com>
2024-10-08 17:41:49 +0330

    fibonacci

--- fibonacci/app.go
+++ fibonacci/app.go
@@ -1,19 +1,13 @@
 package fibonacci
 
-import "fmt"
-
-func Fibonacci() int {
-	var current int = 0
+func Fibonacci() [30]int {
 	var finalItems = [30]int{1, 2}
 
-	fmt.Println(len(finalItems))
 	for i := 2; i <= len(finalItems); i++ {
-		fmt.Println(finalItems[i-2]+finalItems[i-1], "i1")
-
 		finalItems[i] = finalItems[i-2] + finalItems[i-1]
 		if finalItems[len(finalItems)-1] != 0 {
 			break
 		}
 	}
-	return current
+	return finalItems
 }
