39a9e56f91ff0dce30f2199f0426af46a15078ab
ArsalanNury <arsiiiland@yahoo.com>
2024-11-05 00:04:52 +0330

    LargestFactor

--- factor/largestFactor.go
+++ factor/largestFactor.go
@@ -1,5 +1,22 @@
 package factor
 
-func LargestFactor() int {
-	return 0
+// SOLVED BY CHAT_GPT NOT ME
+
+func LargestFactor() int64 {
+	var number int64 = 600851475143
+	//fmt.Println("Largest prime factor:", largestPrimeFactor(number))
+	largestPrimeFactor(number)
+	return number
+}
+
+func largestPrimeFactor(n int64) int64 {
+	var factor int64 = 2
+	for factor*factor <= n {
+		if n%factor == 0 {
+			n /= factor
+		} else {
+			factor++
+		}
+	}
+	return n
 }
