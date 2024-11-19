0909959398408ff32c7c2792cc0d84b1f82c2716
arsalannury <arsiiiland@yahoo.com>
2024-11-19 12:01:12 +0330

    training again and again

--- training/app.go
+++ training/app.go
@@ -15,6 +15,11 @@ type Post struct {
 	content string
 }
 
+type Status struct {
+	lastSee time.Time
+	status  string
+}
+
 func Training() {
 	var firstUserPost Post = Post{
 		createdAt: time.Now(),
@@ -29,12 +34,19 @@ func Training() {
 		},
 	}
 
+	var userStatus Status = Status{
+		lastSee: time.Date(2023, time.January, 2, 12, 25, 10, 1, time.UTC),
+		status:  "online",
+	}
+
 	postDetail(&firstUserPost)
+	statusDetail(userStatus)
 }
 
 func postDetail(model *Post) {
-	fmt.Println((*model).photos)
-	fmt.Println((*model).content)
-	fmt.Println((*model).author)
-	fmt.Println((*model).createdAt)
+	//fmt.Print(model)
+}
+
+func statusDetail(model Status) {
+	fmt.Print("This user was available at ", model.lastSee, " and current status is ", model.status)
 }
