58b060158638c20a4d6f650cd844cc2811263b23
arsalannury <arsiiiland@yahoo.com>
2024-11-20 17:26:47 +0330

    SmallestMultiple

--- smallestMultiple/app.go
+++ smallestMultiple/app.go
@@ -1,26 +1,31 @@
 package smallestMultiple
 
-import "fmt"
+import (
+	"strings"
+)
 
 func SmallestMultiple() {
 	var nums [20]int = [20]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
 	var counter int = 1
 	var index int = 0
+	var passed []bool
 
 	for {
-		if counter == 300 {
+		if len(passed) == len(nums) {
 			break
 		}
 
-		if index == 19 {
+		if index == len(nums) {
 			counter += 1
 			index = 0
+			passed = nil
 		}
 
-		if nums[index]%counter == 0 {
-			index += 1
-			fmt.Println(nums[index] % counter)
+		if !strings.Contains(string(rune(float64(counter/nums[index]))), ".") {
+			passed = append(passed, true)
 		}
+
+		index += 1
 	}
 
 }
